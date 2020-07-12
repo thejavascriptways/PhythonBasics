dcdbf71de47795dfe7547380451311cf9a7f00b1
Deepak Singla <thejavascriptways@gmail.com>
2020-07-12 16:34:44 -0500

    Create functionsAndConditionals.py

---
+++ functionsAndConditionals.py
@@ -0,0 +1,37 @@
+## creating your own functions ##
+
+# def mean(mylist):
+#     the_mean = sum(mylist)/len(mylist)
+#     return the_mean
+# print(mean([1,2,3,4,5]))
+
+## notice the types of the functions and built in funtctions/methods
+# print(type(mean), type(sum))
+
+# def lengther(lst):
+#     number = len(lst)
+#     return number
+# print(lengther([1,2,3,4,5,6,7,8,9]))
+
+# def squareArea(l):
+#     area = l*l
+#     return area
+# print(squareArea(5))
+
+# def ConvertOuncesToMillilitres(v):
+#     vol_in_mill = v * 29.57353
+#     return vol_in_mill
+# print(ConvertOuncesToMillilitres(100))
+
+## Conditionals ##
+
+# def mean(value):
+#     if type(value) == dict:
+#         the_mean = sum(value.values())/len(value)
+#     else:    
+#         the_mean = sum(value)/len(value)
+#     return the_mean
+
+# print(mean([1,2,3,4,5]))
+# print(mean({"morning": 40.5, "noon":90.5,"evening":65.6}))
+
